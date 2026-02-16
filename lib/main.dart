77fa0c86901a20f3f13e24fc14d8e10561471fb1
vijayasree450 <vijayasree450@gmail.com>
2026-02-16 15:04:41 +0530

    add main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MaterialApp(
-  home: Homepage(),
-));
+void main() => runApp(
+      MaterialApp(
+        home: Homepage(),
+      ),
+    );
+
 class Homepage extends StatefulWidget {
   @override
   _HomepageState createState() => _HomepageState();
 }
+
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FD Calculator"),
+        toolbarHeight: 80,
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        leading: Icon(
+          Icons.notes,
+          size: 30,
+          color: Colors.white,
+        ),
+        actions: [
+          Icon(
+            Icons.info,
+            size: 30,
+            color: Colors.white,
+          ),
+        ],
       ),
     );
   }
